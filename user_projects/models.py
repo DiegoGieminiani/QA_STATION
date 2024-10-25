c52919ddefe110a23b523be958400942b1820086
mmorenoo <marianjmo06@gmail.com>
2024-10-25 20:46:14 -0300

    Configuración de la base de datos MySQL y estructura inicial del proyecto

--- user_projects/models.py
+++ user_projects/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Project Name")
+    description = models.TextField(verbose_name="Project Description")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects', verbose_name="Owner")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+        ordering = ['name']
